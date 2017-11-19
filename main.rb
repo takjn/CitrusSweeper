b9b8b86c1feaa15788ae347d2ad018cbf8a9f510
Jun Takeda <takeda.jun@gmail.com>
2017-11-20 00:42:49 +0900

    remove unused code

--- main.rb
+++ main.rb
@@ -71,19 +71,10 @@ class  Tb6612
   
 end
 
-motor = Tb6612.new
-
 Usb = Serial.new(0,115200)
+
+motor = Tb6612.new
 Usb.println("Moter System Start.")
-
-
-#ESP8266を一度停止させる(リセットと同じ)
-pinMode(5,1)
-
-digitalWrite(5,0)   # LOW:Disable
-delay 500
-digitalWrite(5,1)   # LOW:Disable
-delay 500
 
 if(!System.use?('WiFi'))then
   Usb.println "WiFi Card can't use."
@@ -97,8 +88,7 @@ Usb.println WiFi.disconnect
 Usb.println "WiFi Mode Setting"
 Usb.println WiFi.setMode 3  #Station-Mode & SoftAPI-Mode
 
-# Usb.println "WiFi access point"
-# #Usb.println WiFi.softAP "GR-CITRUS","37003700",2,3
+Usb.println "WiFi access point"
 Usb.println WiFi.softAP "Sweeper 192.168.4.1","37003700",2,3
 
 Usb.println "WiFi dhcp enable"
@@ -106,9 +96,6 @@ Usb.println WiFi.dhcp 0,1
 
 Usb.println "WiFi multiConnect Set"
 Usb.println WiFi.multiConnect 1
-
-# Usb.println "WiFi connecting"
-# Usb.println WiFi.connect("Weeyble01","akiba2525")
 
 Usb.println "WiFi ipconfig"
 Usb.println WiFi.ipconfig
@@ -138,24 +125,6 @@ headerbtn += "Content-Length: " + bodybtn.length.to_s + "\r\n\r\n"
 
 Usb.println WiFi.httpServer(80).to_s
 
-#header.txtを読み込む
-def headview
-    return
-    
-  fn = SD.open(0, 'header.txt', 0)
-  if(fn < 0)then
-    Usb.println "SD open error"
-  else
-    size = SD.size(fn)
-    for i in 0..size
-      Usb.print SD.read(fn).chr
-    end
-    SD.close(fn)
-  end
-
-  Usb.println WiFi.connectedIP
-end
-
 while(true)do
   res, sesnum = WiFi.httpServer
   #Usb.println res.to_s
@@ -164,68 +133,53 @@ while(true)do
       
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-
-    headview()
   elsif(res == "/?motor=0")
     Usb.println res + " " + sesnum.to_s
     motor.stop
     led 0
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   elsif(res == "/?motor=1")
     Usb.println res + " " + sesnum.to_s
     motor.forward
     led 1
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   elsif(res == "/?motor=2")
     Usb.println res + " " + sesnum.to_s
     motor.backward
     led 1
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   elsif(res == "/?motor=3")
     Usb.println res + " " + sesnum.to_s
     motor.turn_left
     led 1
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   elsif(res == "/?motor=4")
     Usb.println res + " " + sesnum.to_s
     motor.turn_right
     led 1
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   elsif(res == "/?exit=1")
     Usb.println res + " " + sesnum.to_s
     
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
     break
   elsif(res == "0,CLOSED\r\n")
     Usb.println res + " " + sesnum.to_s
-    
-    headview()
   elsif(res.to_s.length > 2 && ((res.bytes[0].to_s + res.bytes[1].to_s  == "0,") || (res.bytes[0].to_s + res.bytes[1].to_s  == "1,")))
     Usb.println "Else(*,:" + res + " " + sesnum.to_s
-  
-    headview()
   elsif(res != 0)
     Usb.println "Else:" + res.to_s
       
     WiFi.send(sesnum, headerbtn)
     WiFi.send(sesnum, bodybtn)
-    headview()
   end
-  delay 0
 end
 
-#WiFi.send 0, "OK"
 WiFi.httpServer(-1)
 Usb.println WiFi.disconnect
