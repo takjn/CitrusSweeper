a3e95acdea066e5dba863f7d1ab2a1349c3fc4a2
Jun Takeda <takeda.jun@gmail.com>
2017-11-20 01:21:53 +0900

    refactoring

--- main.rb
+++ main.rb
@@ -103,71 +103,72 @@ Usb.println WiFi.ipconfig
 Usb.println WiFi.httpServer(-1).to_s
 delay 100
 
-bodybtn = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
-bodybtn += '<title>CITRUS Sweeper</title></head>'
-bodybtn += '<body><form method="get" >'
-bodybtn += '<h1 align="center">CITRUS Sweeper<br><br><br>'
-bodybtn += "<button type='submit' name='motor' value='1' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Forward</button><br><br>"
-bodybtn += "<button type='submit' name='motor' value='3' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Turn Left</button>"
-bodybtn += "<button type='submit' name='motor' value='0' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Stop</button>"
-bodybtn += "<button type='submit' name='motor' value='4' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Turn Right</button><br><br>"
-bodybtn += "<button type='submit' name='motor' value='2' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Backward</button><br><br>"
-bodybtn += "<button type='submit' name='exit' value='1' style='width: 70%; height: 256px; padding: 20px;font-size:50px;'>EXIT</button><br>"
-bodybtn += "</h1></form></body></html>"
-bodybtn += "\r\n\r\n"
+INDEX_BODY = <<EOS
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>CITRUS Sweeper</title></head>
+<body><form method="get" >
+<h1 align="center">CITRUS Sweeper<br><br><br>
+<button type='submit' name='motor' value='1' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Forward</button><br><br>
+<button type='submit' name='motor' value='3' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Turn Left</button>
+<button type='submit' name='motor' value='0' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Stop</button>
+<button type='submit' name='motor' value='4' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Turn Right</button><br><br>
+<button type='submit' name='motor' value='2' style='width: 30%; height: 256px; padding: 20px;font-size:50px;'>Backward</button><br><br>
+<button type='submit' name='exit' value='1' style='width: 70%; height: 256px; padding: 20px;font-size:50px;'>EXIT</button><br>
+</h1></form></body></html>
+EOS
 
-headerbtn = "HTTP/1.1 200 OK\r\n"
-headerbtn += "Server: GR-CITRUS\r\n"
-headerbtn += "Content-Type: text/html\r\n"
-#headerbtn += "Date: Sun, 22 Oct 2017 12:00:00 GMT\r\n"
-headerbtn += "Connection: close\r\n"
-headerbtn += "Content-Length: " + bodybtn.length.to_s + "\r\n\r\n"
+INDEX_HEADER = <<EOS
+HTTP/1.1 200 OK
+Server: GR-CITRUS
+Content-Type: text/html
+Connection: close
+Content-Length: #{INDEX_BODY.length.to_s}
+
+EOS
 
 Usb.println WiFi.httpServer(80).to_s
 
-while(true)do
+def send_index_html(sesnum)
+  WiFi.send(sesnum, INDEX_HEADER)
+  WiFi.send(sesnum, INDEX_BODY)
+end
+
+loop do
   res, sesnum = WiFi.httpServer
   #Usb.println res.to_s
   if(res == "/")then
     Usb.println res + " " + sesnum.to_s
       
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?motor=0")
     Usb.println res + " " + sesnum.to_s
     motor.stop
     led 0
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?motor=1")
     Usb.println res + " " + sesnum.to_s
     motor.forward
     led 1
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?motor=2")
     Usb.println res + " " + sesnum.to_s
     motor.backward
     led 1
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?motor=3")
     Usb.println res + " " + sesnum.to_s
     motor.turn_left
     led 1
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?motor=4")
     Usb.println res + " " + sesnum.to_s
     motor.turn_right
     led 1
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   elsif(res == "/?exit=1")
     Usb.println res + " " + sesnum.to_s
     
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
     break
   elsif(res == "0,CLOSED\r\n")
     Usb.println res + " " + sesnum.to_s
@@ -176,8 +177,7 @@ while(true)do
   elsif(res != 0)
     Usb.println "Else:" + res.to_s
       
-    WiFi.send(sesnum, headerbtn)
-    WiFi.send(sesnum, bodybtn)
+    send_index_html(sesnum)
   end
 end
 
